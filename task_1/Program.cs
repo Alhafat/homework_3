118409bf67cfe586df883a90d00d6361e60854f4
Alhafat <vladimir-lukin@inbox.ru>
2023-01-22 18:50:53 +0300

    task1

--- task_1/Program.cs
+++ task_1/Program.cs
@@ -9,40 +9,36 @@
 // 23432 -> да
 
 Console.Clear();
-// int number=23432;
-
-int count;
-int count_1;
-int number1;
-// Console.WriteLine(number/100%10);
 
 while (true)
 {
     Console.Write("Введите любое число: ");
     int number=int.Parse(Console.ReadLine()!);
-    if ((number/10000)<1)
+    if ((number/10000)<1) 
     {
         Console.WriteLine($"Запрашиваемое число введено неверно, попробуйте еще раз!");
         continue;
     }
-    else 
+    else
     {
-        while (number1==number/100%10)
+        int result=number;
+        int count;
+        int number1=0;
+        while (number>0)
         {
-            count=number/100;
-            Console.WriteLine($"Count {count}");
-            count_1=number%10;
-            Console.WriteLine($"Count_1 {count_1}");
-            if (count==count_1)
-            {
-                number=number%10/10;
-                Console.WriteLine(number);
-            }
-            else 
-            {   
-                Console.WriteLine($"Введенное число {number} не является палиндромом");
-                break;
-            }
+            count=number%10;
+            number1=number1*10+count;
+            number=number/10;
+        }
+        if (result==number1)
+        {
+            Console.WriteLine($"Введенное число {result} является палиндромом");
+            break;
+        }
+        else
+        {
+            Console.WriteLine($"Введенное число {result} не является палиндромом");
+            break;
         }
     }
 }
