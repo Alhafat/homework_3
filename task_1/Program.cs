67e6ac82e5dc9dfa8a6fb7db1ae1584fa116b233
Alhafat <vladimir-lukin@inbox.ru>
2023-01-22 20:52:21 +0300

    task1

--- task_1/Program.cs
+++ task_1/Program.cs
@@ -10,35 +10,76 @@
 
 Console.Clear();
 
-while (true)
+// while (true)
+// {
+//     Console.Write("Введите любое число: ");
+//     int number=int.Parse(Console.ReadLine()!);
+//     if ((number/10000)<1) 
+//     {
+//         Console.WriteLine($"Запрашиваемое число введено неверно, попробуйте еще раз!");
+//         continue;
+//     }
+//     else
+//     {
+//         int result=number;
+//         int count;
+//         int number1=0;
+//         while (number>0)
+//         {
+//             count=number%10;
+//             number1=number1*10+count;
+//             number=number/10;
+//         }
+//         if (result==number1)
+//         {
+//             Console.WriteLine($"Введенное число {result} является палиндромом");
+//             break;
+//         }
+//         else
+//         {
+//             Console.WriteLine($"Введенное число {result} не является палиндромом");
+//             break;
+//         }
+//     }
+// }
+
+
+        
+Console.Write("Введите любое число: ");
+int number=int.Parse(Console.ReadLine()!);
+int result=number;
+int count=0;
+int numberLast=0;
+int numberFirst=0;
+
+while (number>0)
 {
-    Console.Write("Введите любое число: ");
-    int number=int.Parse(Console.ReadLine()!);
-    if ((number/10000)<1) 
+    numberLast=number%10;
+    Console.WriteLine($"numberLast: {numberLast}");
+    int digits=number;
+    while (digits>0)
     {
-        Console.WriteLine($"Запрашиваемое число введено неверно, попробуйте еще раз!");
-        continue;
+        numberFirst=digits; 
+        digits=digits/10;
+        count++;
+        // Console.WriteLine($"count: {count}");
+        // Console.WriteLine($"numberFirst: {numberFirst}");
+    }
+    // break;
+    count-=1;
+    // Console.WriteLine($"count: {count}");
+    // Console.WriteLine($"numberFirst: {numberFirst}");
+    if (numberFirst==numberLast)
+    {
+        number=number-numberFirst*Convert.ToInt32(Math.Pow(10,count));  //убрали первую цифру
+        // number=number-numberFirst*Math.Pow(10,count);                   //убрали первую цифру
+        number=number/10;                                               // убрали вторую цифру
+        // Console.WriteLine($"number: {number}"); 
     }
     else
     {
-        int result=number;
-        int count;
-        int number1=0;
-        while (number>0)
-        {
-            count=number%10;
-            number1=number1*10+count;
-            number=number/10;
-        }
-        if (result==number1)
-        {
-            Console.WriteLine($"Введенное число {result} является палиндромом");
-            break;
-        }
-        else
-        {
-            Console.WriteLine($"Введенное число {result} не является палиндромом");
-            break;
-        }
+        Console.WriteLine($"Введенное число {result} не является палиндромом");
+        break;
     }
 }
+if (numberFirst==numberLast) Console.WriteLine($"Введенное число {result} является палиндромом");
